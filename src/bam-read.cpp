789ea5d571c037448612c35158a57fb7c6b66d61
Richard Smith-Unna <richardsmith404@gmail.com>
2015-06-25 15:47:21 +0100

    Parallel queue batches of alignments

--- src/bam-read.cpp
+++ src/bam-read.cpp
@@ -1,5 +1,9 @@
 #include "bam-read.h"
 
+#include <thread>
+
+
+
 int BamRead::estimate_fragment_size(std::string file) {
   if (!reader.Open(file)) {
     cerr << "Could not open BAM file" << endl;
@@ -19,6 +23,7 @@ int BamRead::estimate_fragment_size(std::string file) {
   double s = 0;
   bool is_reversed;
   bool is_mate_reversed;
+  BamAlignment alignment;
 
   while (reader.GetNextAlignment(alignment) && count < 10000) {
     if (name != "") {
@@ -64,19 +69,13 @@ int BamRead::estimate_fragment_size(std::string file) {
       }
     }
   }
-  mean=mean/(double)count;
-  // cout << "count: " << count << " mean:" << mean << endl;
+  mean = mean/(double)count;
   s = sqrt(s/(count-1));
-  // cout << "sd: " << s << endl;
   realistic_distance = (int)(3 * s + mean);
   return 0;
 }
 
-int BamRead::load_bam(std::string file) {
-  if (!reader.Open(file)) {
-    cerr << "Could not open BAM file" << endl;
-    return 1;
-  }
+void BamRead::load_bam_header() {
 
   // load the sam header
   SamSequenceDictionary dictionary = reader.GetHeader().Sequences;
@@ -87,98 +86,171 @@ int BamRead::load_bam(std::string file) {
   // fill the hash map with initial values
   std::vector<SamSequence>::iterator it = dictionary.Begin();
   int len = 0;
+
   for (int i = 0; i < seq_count; i++) {
+
     if (it[i].HasLength()) {
       len = atoi(it[i].Length.c_str());
     } else {
       len = 0;
     }
-    // array[i] = TransratePileup(it[i].Name, len);
+
     array[i].setName(it[i].Name);
     array[i].setLength(len);
+
   }
 
-  // loop through the bam file
-  double scale = 0.65;
-  double seq_true = 0;
-  while (reader.GetNextAlignment(alignment)) {
-    // read must be mapped
+}
+
+void BamRead::load_bam(std::string file, int n_threads) {
+
+  if (!reader.Open(file)) {
+    cerr << "Could not open BAM file" << endl;
+    exit(1);
+  }
+
+  load_bam_header();
+
+  vector<BatchQueue> queues;
+  vector<std::thread> threads;
+  for (int i = 0; i < n_threads; ++i) {
+    BatchQueue queue;
+    queues.emplace_back(queue);
+    threads.emplace_back( std::thread(process_queue, this, queue) );
+  }
+
+
+  vector<Batch> batches(queues.size());
+
+  // send alignments to parallel processing threads in batches
+  while (true) {
+
+    BamAlignment alignment;
+    if (!reader.GetNextAlignment(alignment)) {
+      break;
+    }
+
     if (!alignment.IsMapped()) {
       continue;
     }
-    refid = alignment.RefID;
-    ++array[refid].reads_mapped;
 
-    array[refid].addAlignment(alignment);
+    int queue_no = alignment.RefID % n_threads;
+    Batch batch = batches[queue_no];
 
-    // store edit distance for sequence accuracy calculation
-    if (alignment.HasTag("NM")) {
-      if (alignment.GetTag("NM", nm_tag)) {
-        len = alignment.Length;
-        scale = (len - 35)/(double)len;
-        seq_true = (len - nm_tag)/(double)len;
-        seq_true = (seq_true - scale) * (1 / (1 - scale));
-        array[refid].p_seq_true += seq_true;
-      }
+    if (batch.size() == 1e5) {
+
+      // this batch is ready to be processed - add it to queue
+      queues[queue_no].enqueue(batch);
+
+      // start a fresh batch for this queue
+      Batch newbatch;
+      batches[queue_no] = newbatch;
+
+    } else {
+
+      // just keep populating the batch
+      batch.emplace_back(alignment);
+
     }
 
-    // count fragments where either or both mates mapped
-    if (alignment.IsFirstMate() ||
-      (alignment.IsSecondMate() && !alignment.IsMateMapped())) {
-      ++array[refid].fragments_mapped;
-    }
-
-    // from now on ignore fragments unless both mates mapped
-    if (!(alignment.IsFirstMate() && alignment.IsMateMapped())) {
-      continue;
-    }
-
-    ++array[refid].both_mapped;
-
-    // // count proper pairs, although we have our own definition because
-    // // not all aligners use the same definition
-    if (alignment.IsProperPair()) {
-      ++array[refid].properpair;
-    }
-
-    // // mates must align to same contig, otherwise we record a bridge
-    if (refid != alignment.MateRefID) {
-      ++array[refid].bridges;
-      continue;
-    }
-
-    ldist = max(alignment.Position-alignment.MatePosition,
-                alignment.MatePosition-alignment.Position);
-    if (ldist > realistic_distance) {
-      // mates are too far apart
-      continue;
-    }
-
-    // read orientation must match the generated library
-    // in this case we only test for FR/RF orientation,
-    // that is - we expect mates to be on opposite strands
-    bool is_reversed = alignment.IsReverseStrand();
-    bool is_mate_reversed = alignment.IsMateReverseStrand();
-
-    if (!is_reversed && is_mate_reversed) {
-      // in FR orientation, first read must start
-      // before second read
-      if (alignment.Position < alignment.MatePosition) {
-        array[refid].good++;
-      }
-    } else if (is_reversed && !is_mate_reversed) {
-      // in RF orientation, second read must start
-      // before first read
-      if (alignment.MatePosition < alignment.Position) {
-        array[refid].good++;
-      }
-    }
-  } // end of bam file
-  for (int i = 0; i < seq_count; i++) {
-    array[i].calculateUncoveredBases();
-    array[i].setPNotSegmented();
   }
-  return 0;
+
+  // process any batches that didn't fill up completely
+  for (int i = 0; i < batches.size(); ++i) {
+    if (batches[i].size() > 0) {
+      queues[i].enqueue(batches[i]);
+    }
+    Batch batch;
+    queues[i].enqueue(batch);
+  }
+
+  // wait for threads to finish
+  for (auto &thread : threads) {
+    thread.join();
+  }
+  threads.clear();
+
+  // process contigs
+  for (int i = 0; i < n_threads; ++i) {
+    threads.emplace_back( std::thread(process_contigs, this, i) );
+  }
+  for (auto &thread : threads) {
+    thread.join();
+  }
+
+}
+
+void BamRead::process_alignment(BamAlignment alignment) {
+  refid = alignment.RefID;
+  ++array[refid].reads_mapped;
+
+  array[refid].addAlignment(alignment);
+
+  int len = alignment.Length;
+  double scale = 0.65;
+  double seq_true = 0;
+
+  // store edit distance for sequence accuracy calculation
+  if (alignment.HasTag("NM")) {
+    if (alignment.GetTag("NM", nm_tag)) {
+      scale = (len - 35)/(double)len;
+      seq_true = (len - nm_tag)/(double)len;
+      seq_true = (seq_true - scale) * (1 / (1 - scale));
+      array[refid].p_seq_true += seq_true;
+    }
+  }
+
+  // count fragments where either or both mates mapped
+  if (alignment.IsFirstMate() ||
+    (alignment.IsSecondMate() && !alignment.IsMateMapped())) {
+    ++array[refid].fragments_mapped;
+  }
+
+  // from now on ignore fragments unless both mates mapped
+  if (!(alignment.IsFirstMate() && alignment.IsMateMapped())) {
+    return;
+  }
+
+  ++array[refid].both_mapped;
+
+  // // count proper pairs, although we have our own definition because
+  // // not all aligners use the same definition
+  if (alignment.IsProperPair()) {
+    ++array[refid].properpair;
+  }
+
+  // // mates must align to same contig, otherwise we record a bridge
+  if (refid != alignment.MateRefID) {
+    ++array[refid].bridges;
+    return;
+  }
+
+  ldist = max(alignment.Position-alignment.MatePosition,
+              alignment.MatePosition-alignment.Position);
+  if (ldist > realistic_distance) {
+    // mates are too far apart
+    return;
+  }
+
+  // read orientation must match the generated library
+  // in this case we only test for FR/RF orientation,
+  // that is - we expect mates to be on opposite strands
+  bool is_reversed = alignment.IsReverseStrand();
+  bool is_mate_reversed = alignment.IsMateReverseStrand();
+
+  if (!is_reversed && is_mate_reversed) {
+    // in FR orientation, first read must start
+    // before second read
+    if (alignment.Position < alignment.MatePosition) {
+      array[refid].good++;
+    }
+  } else if (is_reversed && !is_mate_reversed) {
+    // in RF orientation, second read must start
+    // before first read
+    if (alignment.MatePosition < alignment.Position) {
+      array[refid].good++;
+    }
+  }
 }
 
 int main (int argc, char* argv[]) {
@@ -191,7 +263,8 @@ int main (int argc, char* argv[]) {
     }
     string infile = argv[1];
     bam.estimate_fragment_size(infile);
-    bam.load_bam(infile);
+    bam.load_bam(infile, 4);
+
     // open file for writing
     std::ofstream output;
     output.open (argv[2]);
@@ -219,13 +292,17 @@ int main (int argc, char* argv[]) {
     output.close();
 
     return 0;
+
   } else {
+
     cout << "bam-read version 1.0.0\n"
          << "Usage:\n"
          << "bam-read <bam_file> <output_csv> <nullprior (optional)>\n\n"
          << "example:\n"
          << "bam-read in.bam out.csv 0.95"
          << endl;
+
     return 1;
+
   }
 }
--- src/bam-read.h
+++ src/bam-read.h
@@ -5,10 +5,17 @@
 #include <vector>
 #include "api/BamReader.h"
 #include "pileup.h"
+#include "atomicops.h"
+#include "readerwriterqueue.h"
 
 using namespace BamTools;
 
 using namespace std;
+
+using namespace moodycamel;
+
+typedef vector<BamAlignment> Batch;
+typedef BlockingReaderWriterQueue<Batch> BatchQueue;
 
 double nullprior = 0.7;
 
@@ -16,7 +23,9 @@ class BamRead
 {
 public:
   int estimate_fragment_size(std::string file);
-  int load_bam(std::string);
+  void load_bam(std::string, int n_threads);
+  void load_bam_header();
+  void process_alignment(BamAlignment alignment);
   int bar;
   int seq_count;
   uint32_t nm_tag;
@@ -24,6 +33,33 @@ public:
   int realistic_distance;
   int refid;
   BamReader reader;
-  BamAlignment alignment;
   std::vector<TransratePileup> array;
 };
+
+// function that each thread runs
+// this function gets passed a queue and just processes it until
+// it gets an empty batch of alignments
+void process_queue(BamRead &br, BatchQueue &queue, bool done) {
+
+  Batch batch;
+  queue.wait_dequeue(batch);
+  while (batch.size() > 0) {
+    for (auto &alignment : batch) {
+      br.process_alignment(alignment);
+    }
+    // free up the memory for this batch
+    batch.clear();
+    queue.wait_dequeue(batch);
+  }
+
+}
+
+// do final summary processing of each contig
+void process_contigs(BamRead &br, int thread_id) {
+  for (int i = 0; i < br.seq_count; ++i) {
+    if (i % thread_id == 0) {
+      br.array[i].calculateUncoveredBases();
+      br.array[i].setPNotSegmented();
+    }
+  }
+}
